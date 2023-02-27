8fc0326e6eb82686b6dc4d6f429c15509c9d3e60
Baku Hashimoto <mail@baku89.com>
2023-02-27 17:10:16 +0900

    Update bk_PackKeyframes

--- 00 Composition/bk_PackKeyframes.jsx
+++ 00 Composition/bk_PackKeyframes.jsx
@@ -4,19 +4,30 @@
 run( function() {
 
     var comp = getActiveComp();
-	var layers = getSelectedLayers();
+    var layers = getSelectedLayers();    
    
     var layer = layers[0];
+    var timeRemap = layer.timeRemap;
+
+    if (timeRemap.selectedKeys.length == 0) {
+		throw "No keyframe selected.";
+		return;
+	}
     
     var values = [];
+
+    selectedKeys = timeRemap.selectedKeys.slice(0);
     
-    for ( var i = layer.timeRemap.numKeys; i > 0; i-- ) {
-        
-        values.push( layer.timeRemap.keyValue( i ) );
+    for (var i = selectedKeys.length - 1; i >= 0; i--) {
+        var key = selectedKeys[i];
+        values.push(timeRemap.keyValue(key));
+        timeRemap.removeKey(key);
     }
 
+    values = values.reverse();
+
     for ( var i = 0; i < values.length; i ++ ) {
-        layer.timeRemap.setValueAtTime( comp.frameDuration * i, values[i] );
+        layer.timeRemap.setValueAtTime(comp.time + comp.frameDuration * i, values[i]);
     }
 
 
